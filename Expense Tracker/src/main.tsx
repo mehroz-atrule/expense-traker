a7556334ddbcfe940c7fc0c7ce0005e91ff687f6
Mehroz101 <mehroz.farooq@atrule.com>
2025-10-27 13:09:37 +0500

    Update main.tsx

--- Expense Tracker/src/main.tsx
+++ Expense Tracker/src/main.tsx
@@ -5,8 +5,8 @@ import store from './app/store'
 import App from './App'
 import { ToastProvider } from './components/Toast'
 import './main.css'
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
